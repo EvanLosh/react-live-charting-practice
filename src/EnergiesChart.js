05d5a4c667cdd393b534edd370b8663b7430cc66
Evan Losh <evand.losh@gmail.com>
2023-12-04 06:58:17 -0500

    starting over

---
+++ src/EnergiesChart.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from "react"
+import Chart from 'chart.js/auto'
+
+
+
+function EnergiesChart() {
+
+    const ctx = document.getElementById('histogram')
+
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default EnergiesChart
