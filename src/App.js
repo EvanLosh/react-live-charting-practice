0679a464e8dd9199d08dd09a91c077e2a1ceb3a7
Evan Losh <evand.losh@gmail.com>
2023-12-04 09:48:04 -0500

    finishing up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import './App.css';
-import EnergiesChart from "./EnergiesChart.js"
+import './App.css'
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+import { useRef } from 'react'
 
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend)
 
+// Create some random data
+let data = []
+function generateData() {
+  data.length = 0
+  for (let i = 0; i < 4; i++) {
+    data.push(10 * Math.random())
+  }
+}
+
+// Configure data for the chart
+const dataConfig = {
+  labels: ['Mon', 'Tues', 'Wed', 'Thurs'],
+  datasets: [
+    {
+      label: 'Random numbers',
+      data: data,
+      backgroundColor: 'aqua',
+      borderColor: 'black',
+      borderWidth: 1
+    }
+  ]
+}
 
 function App() {
 
+  generateData()
 
+  // We will use this variable to access the chart in the DOM
+  const theChart = useRef(null)
 
+  function updateChart() {
+    generateData()
+    theChart.current.update() // Calling the chart's update method
+  }
 
   return (
     <div className="App">
-      <EnergiesChart />
+      <Bar
+        data={dataConfig}
+        options={null}
+        ref={theChart} // When Bar gets rendered, theChart gets assigned to its DOM element. This allows us to access the chart's update method.
+      />
+      <button onClick={updateChart}>Update the chart</button>
     </div>
   );
 }
--- src/EnergiesChart.js
+++ src/EnergiesChart.js
@@ -1,12 +1,25 @@
 import './App.css';
 import React from "react"
 import Chart from 'chart.js/auto'
+import ReactDOM from 'react-dom/client';
 
 
 
 function EnergiesChart() {
 
-    const ctx = document.getElementById('histogram')
+    const data = [1, 1, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9]
+
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    }
 
     return (
         <div>
